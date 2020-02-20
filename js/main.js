2c8deb8c49c7b600cf2b19340a2327ade0b95857
Alisa <alisa.93_93@mail.ru>
2020-02-20 19:43:53 +0300

    Личный проект: доверяй, но проверяй (часть 1)

--- js/main.js
+++ js/main.js
@@ -10,6 +10,9 @@ var MIN_X_LOCATION = 0;
 var MAX_X_LOCATION = 1140;
 var MIN_Y_LOCATION = 130;
 var MAX_Y_LOCATION = 630;
+var RADIX = 10;
+var MAINPIN_HEIGHT = 22;
+var ENTER = 'Enter';
 
 var getRandomValue = function (values) {
   return values[Math.floor(Math.random() * values.length)];
@@ -65,6 +68,7 @@ var offers = createSimilarPinsArray(OFFER_COUNT);
 var similarTemplatePin = document.querySelector('#pin')
 .content
 .querySelector('.map__pin');
+var map = document.querySelector('.map');
 
 
 var renderSimilarPin = function (similarPin) {
@@ -91,7 +95,6 @@ var renderPins = function (pins) {
 
 renderPins(offers);
 
-document.querySelector('.map').classList.remove('map--faded');
 var similarTemplateCard = document.querySelector('#card')
 .content
 .querySelector('.map__card');
@@ -222,3 +225,114 @@ var renderCard = function (cards) {
   elementBeforeCard.before(fragment);
 };
 renderCard(offers[0]);
+
+
+var fieldsetHeader = document.querySelector('.ad-form-header');
+var fieldsetInput = document.querySelectorAll('.ad-form__element');
+var adForm = document.querySelector('.ad-form');
+var adSubmit = adForm.querySelector('.ad-form__submit');
+var fieldsetAddress = document.querySelector('#address');
+var mapFilters = document.querySelector('.map__filters');
+var mapPinMain = document.querySelector('.map__pin--main');
+var fieldsetCapacity = document.querySelector('#capacity');
+var roomNumber = document.querySelector('#room_number');
+
+var getCoordinates = function (point, size) {
+  return parseInt(point, RADIX) + Math.round(size * 0.5);
+};
+
+var mainPinX = getCoordinates(mapPinMain.style.left, mapPinMain.offsetWidth);
+var mainPinY = getCoordinates(mapPinMain.style.top, mapPinMain.offsetHeight);
+var mainPinActiveY = mainPinY + MAINPIN_HEIGHT;
+var mainPinCoordinate = mainPinX + ',' + mainPinY;
+var mainPinCoordinateActive = mainPinX + ',' + mainPinActiveY;
+
+var toggleElementAvailability = function (elements, status) {
+  for (var i = 0; i < elements.length; i++) {
+    elements[i].disabled = status;
+  }
+};
+
+var deactivateMap = function () {
+
+  fieldsetHeader.setAttribute('disabled', 'disabled');
+  fieldsetAddress.setAttribute('value', mainPinCoordinate);
+  mapFilters.classList.add('map__filters--disabled');
+  toggleElementAvailability(fieldsetInput, true);
+};
+
+var activatePageHandler = function (evt) {
+  if (evt.which === 1 || evt.key === ENTER) {
+    renderPins();
+    mapPinMain.removeEventListener('mousedown', activatePageHandler);
+    mapPinMain.removeEventListener('keydown', activatePageHandler);
+  }
+
+  map.classList.remove('map--faded');
+  fieldsetHeader.removeAttribute('disabled');
+  adForm.classList.remove('ad-form--disabled');
+  mapFilters.classList.remove('map__filters--disabled');
+  fieldsetAddress.setAttribute('value', mainPinCoordinateActive);
+  toggleElementAvailability(fieldsetInput, false);
+};
+
+mapPinMain.addEventListener('mousedown', activatePageHandler);
+mapPinMain.addEventListener('keydown', activatePageHandler);
+
+var findDisabledElement = function () {
+
+  var index = fieldsetCapacity.options.selectedIndex;
+  roomNumber.options[index].selected = true;
+  var roomNumberLenght = roomNumber.options.length;
+
+  if (index === roomNumberLenght - 1) {
+    for (var j = 0; j < roomNumberLenght; j++) {
+      roomNumber.options[j].disabled = j < index;
+    }
+  } else {
+    for (var k = 0; k < roomNumberLenght; k++) {
+      roomNumber.options[k].disabled = k > index;
+    }
+  }
+};
+
+fieldsetCapacity.addEventListener('change', function () {
+  findDisabledElement();
+});
+
+roomNumber.addEventListener('change', function () {
+
+  var index = roomNumber.options.selectedIndex;
+  fieldsetCapacity.options[index].selected = true;
+  var capacityLenght = fieldsetCapacity.options.length;
+
+  for (var k = 0; k < capacityLenght - 1; k++) {
+    if (index === capacityLenght - 1) {
+      fieldsetCapacity.options[index].disabled = false;
+      fieldsetCapacity.options[k].disabled = true;
+    } else {
+      fieldsetCapacity.options[capacityLenght - 1].disabled = true;
+      fieldsetCapacity.options[k].disabled = k < index;
+    }
+  }
+});
+
+var checkRoomsCapacity = function (rooms, capacity) {
+  if (rooms === '1' && capacity === '1') {
+    roomNumber.setCustomValidity('');
+  } else if (rooms === '2' && (capacity < '3' && capacity > '0')) {
+    roomNumber.setCustomValidity('');
+  } else if (rooms === '3' && (capacity <= '3' && capacity > '0')) {
+    roomNumber.setCustomValidity('');
+  } else if (rooms === '100' && capacity === '0') {
+    roomNumber.setCustomValidity('не для гостей');
+  } else {
+    roomNumber.setCustomValidity('Не верное количество комнат или гостей');
+  }
+};
+
+adSubmit.addEventListener('click', function () {
+  checkRoomsCapacity(roomNumber.value, fieldsetCapacity.value);
+});
+
+deactivateMap();
